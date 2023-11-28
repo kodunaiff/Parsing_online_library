e0e8a850a664b4056b4ba0dd77567a9c86e54606
kodunaiff <kodunaiff@gmail.com>
2023-11-29 00:11:06 +0800

    download comments

--- main.py
+++ main.py
@@ -50,8 +50,23 @@ def download_image(link, book_id, folder='images/'):
         file.write(response_image.content)
 
 
+def download_comments(link, book_id):
+    url = f'{link}b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    comments = []
+    soup = BeautifulSoup(response.text, 'lxml')
+    ing_com = soup.find_all('div', class_='texts')
+    for i in ing_com:
+        tex = i.find('span').text
+        if tex:
+            comments.append(tex)
+    return comments
+
+
 os.makedirs('books', exist_ok=True)
 os.makedirs('images', exist_ok=True)
+os.makedirs('comments', exist_ok=True)
 books_count = 10
 library_books = []
 main_url = "https://tululu.org/"
@@ -66,7 +81,8 @@ for book_id in range(1, books_count + 1):
     try:
         mistake_book = check_for_redirect(response.history)
         name_b, author_b = call_book(main_url, book_id)
-        download_txt(response.url, name_b)
-        download_image(main_url, book_id)
+        #download_txt(response.url, name_b)
+        #download_image(main_url, book_id)
+        print(download_comments(main_url, book_id))
     except requests.exceptions.HTTPError:
         continue
