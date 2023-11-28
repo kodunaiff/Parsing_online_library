5363cfa82761af42e9e4a4d2a4295634e1e9c3d2
kodunaiff <kodunaiff@gmail.com>
2023-11-29 03:57:00 +0800

    parse_book_page

--- main.py
+++ main.py
@@ -12,68 +12,58 @@ def check_for_redirect(response_id):
         raise requests.exceptions.HTTPError
 
 
-def call_book(link, book_id):
-    url = f'{link}b{book_id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.find('h1')
-    title_text = title_tag.text
-    title_text_1 = title_text.split('::')
-    return f'{book_id}.{title_text_1[0].strip()}', title_text_1[1].strip()
-
-
 def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
-    folder_f = sanitize_filename(folder)
-    filename_f = sanitize_filename(filename)
-    save_path = os.path.join(folder_f, f'{filename_f}.txt')
+    folder = sanitize_filename(folder)
+    filename = sanitize_filename(filename)
+    save_path = os.path.join(folder, f'{filename}.txt')
     with open(save_path, 'wb') as file:
         file.write(response.content)
 
 
-def download_image(link, book_id, folder='images/'):
-    url = f'{link}b{book_id}/'
-    response = requests.get(url)
+def download_image(image_link, folder='images/'):
+    response = requests.get(image_link)
     response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    image_name = soup.find('div', class_='bookimage').find('img')['src']
-    image_link = urljoin(url, image_name)
-    response_image = requests.get(image_link)
-    response_image.raise_for_status()
     image_part = urlsplit(image_link)
-    url_parts = image_part.path
-    filename, file_extension = os.path.splitext(url_parts)
+    filename, file_extension = os.path.splitext(image_part.path)
     image_filename = filename.split('/')
     save_path = os.path.join(f'{folder}{image_filename[-1]}{file_extension}')
     with open(save_path, 'wb') as file:
-        file.write(response_image.content)
+        file.write(response.content)
 
 
-def download_comments(link, book_id):
+def parse_book_page(link, book_id):
     url = f'{link}b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
-    comments = []
-    genres = []
     soup = BeautifulSoup(response.text, 'lxml')
-    ing_com = soup.find_all('div', class_='texts')
-    for i in ing_com:
-        tex = i.find('span').text
-        if tex:
-            comments.append(tex)
+    book_name, book_author = soup.find('h1').text.split('::')
+    image_name = soup.find('div', class_='bookimage').find('img')['src']
+    image_link = urljoin(url, image_name)
+    book_genre = []
+    genres = soup.find('span', class_='d_book').find_all('a')
+    for genre in genres:
+        book_genre.append(genre.text)
+    book_comments = []
+    comments = soup.find_all('div', class_='texts')
+    for comment in comments:
+        book_comments.append(comment.find('span').text)
 
-    genres_info = soup.find('span', class_='d_book').find_all('a')
-    for i in genres_info:
-        genres.append(i.text)
-    return comments, genres
+    book_information = {
+        'book_name': f'{book_id}. {book_name.strip()}',
+        'book_author': book_author.strip(),
+        'book_genre': book_genre,
+        'book_comments': book_comments,
+        'image_link': image_link,
+    }
+
+    return book_information
 
 
 os.makedirs('books', exist_ok=True)
 os.makedirs('images', exist_ok=True)
-os.makedirs('comments', exist_ok=True)
+
 books_count = 10
-library_books = []
 main_url = "https://tululu.org/"
 for book_id in range(1, books_count + 1):
     url = f"{main_url}txt.php"
@@ -85,9 +75,11 @@ for book_id in range(1, books_count + 1):
 
     try:
         mistake_book = check_for_redirect(response.history)
-        name_b, author_b = call_book(main_url, book_id)
-        #download_txt(response.url, name_b)
-        #download_image(main_url, book_id)
-        print(download_comments(main_url, book_id))
+        descript_book = parse_book_page(main_url, book_id)
+        book_name = descript_book['book_name']
+        image_link = descript_book['image_link']
+        download_txt(response.url, book_name)
+        download_image(image_link)
+
     except requests.exceptions.HTTPError:
         continue
