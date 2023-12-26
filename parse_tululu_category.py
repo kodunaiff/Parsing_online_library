7bc00dcbe2b0617eb22b2d9ccf6588c2cb93f6cf
kodunaiff <kodunaiff@gmail.com>
2023-12-27 00:12:23 +0800

    add new arguments and 'if main'

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,13 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlsplit
-from main import download_txt, download_image, check_for_redirect, parse_book_page
-import os
+import argparse
+import json
 import logging
+import os
 import sys
 from time import sleep
-import json
-import argparse
+from urllib.parse import urljoin, urlsplit
+
+import requests
+from bs4 import BeautifulSoup
+
+from main import download_txt, download_image, check_for_redirect, parse_book_page
 
 
 def create_pages():
@@ -28,57 +30,84 @@ def create_pages():
         type=int,
         default=2
     )
+    parser.add_argument(
+        '--skip_imgs',
+        action='store_true',
+        help='не скачивать картинки'
+    )
+    parser.add_argument(
+        '--skip_txt',
+        action='store_true',
+        help='не скачивать текст'
+    )
+    parser.add_argument(
+        '--dest_folder',
+        type=str,
+        help='путь к каталогу с результатами парсинга: картинкам, книгам, JSON',
+        default='library_folder/'
+    )
+
     args = parser.parse_args()
     start_page = args.start_page
     end_page = args.end_page
-    return start_page, end_page
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+    dest_folder = args.dest_folder
+    return start_page, end_page, skip_imgs, skip_txt, dest_folder
 
 
+def main():
+    start_page, end_page, skip_imgs, skip_txt, library_folder = create_pages()
+    os.makedirs(f'{library_folder}', exist_ok=True)
+    last_page = max(start_page, end_page)
+    library = []
 
-os.makedirs('books', exist_ok=True)
-os.makedirs('images', exist_ok=True)
+    while start_page <= last_page:
+        url = f'https://tululu.org/l55/{start_page}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        start_page += 1
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_selector = 'div.bookimage a'
+        books_number = soup.select(books_selector)
+        for book_number in books_number:
+            book = book_number['href']
+            book_id = book[2:-1]
+            book_link = urljoin(url, book)
+            book_url = urlsplit(url)
+            download_url = f'https://{book_url.netloc}/txt.php'
+            payload = {
+                'id': f'{book_id}',
+            }
+            try:
+                response_book = requests.get(download_url, params=payload)
+                response_book.raise_for_status()
+                check_for_redirect(response_book)
+                book_response = requests.get(book_link)
+                book_response.raise_for_status()
+                check_for_redirect(book_response)
+                book_characteristic = parse_book_page(book_response, book_id)
+                book_name = book_characteristic['book_name']
+                image_link = book_characteristic['image_link']
+                library.append(book_characteristic)
+                if not skip_txt:
+                    download_txt(response_book, book_name, f'{library_folder}')
+                    print(f'{book_id}. book downloaded')
+                if not skip_imgs:
+                    download_image(image_link, f'{library_folder}')
 
-start_page, end_page = create_pages()
-last_page = max(start_page, end_page)
-library = []
-while start_page <= last_page:
-    url = f'https://tululu.org/l55/{start_page}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    start_page += 1
-    soup = BeautifulSoup(response.text, 'lxml')
-    books_selector = 'div.bookimage a'
-    books_number = soup.select(books_selector)
-    for book_number in books_number:
-        book = book_number['href']
-        book_id = book[2:-1]
-        book_link = urljoin(url, book)
-        book_url = urlsplit(url)
-        download_url = f'https://{book_url.netloc}/txt.php'
-        payload = {
-            'id': f'{book_id}',
-        }
-        try:
-            response_book = requests.get(download_url, params=payload)
-            response_book.raise_for_status()
-            check_for_redirect(response_book)
-            book_response = requests.get(book_link)
-            book_response.raise_for_status()
-            check_for_redirect(book_response)
-            book_characteristic = parse_book_page(book_response, book_id)
-            book_name = book_characteristic['book_name']
-            image_link = book_characteristic['image_link']
-            download_txt(response_book, book_name)
-            download_image(image_link)
-            library.append(book_characteristic)
-            print(f'{book_id}. book downloaded')
-        except requests.exceptions.HTTPError:
-            logging.warning(f'{book_id}. book is missing'),
-            # continue
-        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-            print(f"{book_id}. Отсутствие соединения, ожидание 5сек...", file=sys.stderr)
-            sleep(5)
 
-library_json = json.dumps(library, ensure_ascii=False).encode('utf8')
-with open("library.json", "w", encoding='utf8') as my_file:
-    my_file.write(library_json.decode())
+            except requests.exceptions.HTTPError:
+                logging.warning(f'{book_id}. book is missing')
+
+            except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+                print(f"{book_id}. Отсутствие соединения, ожидание 5сек...", file=sys.stderr)
+                sleep(5)
+
+    library_json = json.dumps(library, ensure_ascii=False).encode('utf8')
+    with open(f"{library_folder}/library.json", "w", encoding='utf8') as my_file:
+        my_file.write(library_json.decode())
+
+
+if __name__ == '__main__':
+    main()
